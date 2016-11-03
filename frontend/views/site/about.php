07d80b8d4e8864b6796a77e8d4e09db9ddd3c5d0
root <root@S519988.domain>
2016-11-03 11:03:35 +0800

    change the about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,7 +10,59 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+<!--    <p>This is the About page. You may modify the following file to customize its content:</p>
 
-    <code><?= __FILE__ ?></code>
+    <code><?= __FILE__ ?></code>-->
+    <div class="about-page main grid-wrap">
+
+            <header class="grid col-full">
+            <hr>
+                    <p class="fleft">About</p>
+            </header>
+
+
+
+            <aside class="grid col-one-quarter mq2-col-full">
+                    <p class="mbottom">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis.
+                    </p>
+                    <menu>
+                            <ul>
+                                    <li><a href="#navteam" class="arrow">Our team</a></li>
+                                    <li><a href="#navphilo" class="arrow">Our philosophy</a></li>
+                                    <li><a href="#navplace" class="arrow">Our place</a></li>
+                                    <li><a href="#navother" class="arrow">Our lorem</a></li>
+                            </ul>
+                    </menu>
+            </aside>
+
+            <section class="grid col-three-quarters mq2-col-full">
+                    <img src="img/team.jpg" alt="" >
+
+                    <article id="navteam">
+                    <h2>Our team</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    </article>
+
+                    <article id="navphilo">
+                    <h2>Our philosophy</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    </article>
+
+                    <article id="navplace">
+                    <h2>Our place</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    </article>
+
+                    <article id="navother">
+                    <h2>Our lorem</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit.</p>
+                    </article>
+
+            </section>
+
+    </div> <!--main-->
 </div>
