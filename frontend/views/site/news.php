49972e6e7803324a396dc453d7a2eb879ff97ecc
root <root@S519988.domain>
2016-11-03 11:39:05 +0800

    add news.php

---
+++ frontend/views/site/news.php
@@ -0,0 +1,142 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'News';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-news">
+    <h1><?= Html::encode($this->title) ?></h1>
+    	<div class="blog-page main grid-wrap">
+
+            <header class="grid col-full">
+                    <hr>
+                    <p class="fleft">Blog</p>
+            </header>
+
+
+            <section class="grid col-three-quarters mq2-col-two-thirds mq3-col-full">
+
+                    <article class="post">
+                            <h2><a href="blogpost.html" class="post-title">Blog post page!</a></h2>
+                            <div class="meta">
+                                    <p>Posted on <span class="time">November 15, 2011</span> by <a href="#" class="fn">Sylvain Lafitte</a> in <a href="#"class="cat">Other</a> with <a href="#" class="comments-link">42 comments</a>.</p>
+                            </div>
+                            <div class="entry">
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p>
+                            </div>
+                            <footer>
+                                    <a href="blogpost.html" class="more-link">Continue reading…</a>
+                            </footer>
+                    </article>
+
+                    <article class="post">
+                            <h2><a href="#" class="post-title">Blog post</a></h2>
+                            <div class="meta">
+                                    <p>Posted on <span class="time">November 15, 2011</span> by <a href="#" class="fn">Sylvain Lafitte</a> in <a href="#"class="cat">Other</a> with <a href="#" class="comments-link">42 comments</a>.</p>
+                            </div>
+                            <div class="entry">
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p>
+                            </div>
+                            <footer>
+                                    <a href="#" class="more-link">Continue reading…</a>
+                            </footer>
+                    </article>
+
+                    <article class="post">
+                            <h2><a href="#" class="post-title">Blog post</a></h2>
+                            <div class="meta">
+                                    <p>Posted on <span class="time">November 15, 2011</span> by <a href="#" class="fn">Sylvain Lafitte</a> in <a href="#"class="cat">Other</a> with <a href="#" class="comments-link">42 comments</a>.</p>
+                            </div>
+                            <div class="entry">
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p>
+                            </div>
+                            <footer>
+                                    <a href="#" class="more-link">Continue reading…</a>
+                            </footer>
+                    </article>
+
+                    <article class="post">
+                            <h2><a href="#" class="post-title">Blog post</a></h2>
+                            <div class="meta">
+                                    <p>Posted on <span class="time">November 15, 2011</span> by <a href="#" class="fn">Sylvain Lafitte</a> in <a href="#"class="cat">Other</a> with <a href="#" class="comments-link">42 comments</a>.</p>
+                            </div>
+                            <div class="entry">
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p>
+                            </div>
+                            <footer>
+                                    <a href="#" class="more-link">Continue reading…</a>
+                            </footer>
+                    </article>
+
+                    <article class="post">
+                            <h2><a href="#" class="post-title">Blog post</a></h2>
+                            <div class="meta">
+                                    <p>Posted on <span class="time">November 15, 2011</span> by <a href="#" class="fn">Sylvain Lafitte</a> in <a href="#"class="cat">Other</a> with <a href="#" class="comments-link">42 comments</a>.</p>
+                            </div>
+                            <div class="entry">
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p>
+                            </div>
+                            <footer>
+                                    <a href="#" class="more-link">Continue reading…</a>
+                            </footer>
+                    </article>			
+
+                    <ul class="page-numbers">
+                            <li><a href="#">Prev</a></li>
+                            <li><a href="#" class="current">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">Next</a></li>
+                    </ul>
+
+            </section>
+
+            <aside class="grid col-one-quarter mq2-col-one-third mq3-col-full blog-sidebar">
+
+                    <div class="widget">
+                            <input id="search" type="search" name="search" value="Type and hit enter to search" >
+                    </div>
+
+                    <div class="widget">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim.</p>
+                    </div>
+
+                    <div class="widget">
+                    <h2>Popular Posts</h2>
+                    <ul>
+                            <li><a href="#" title="">Nullam porttitor elementum ligula</a></li>
+                            <li><a href="#" title="">Vestibulum interdum</a></li>	
+                            <li><a href="#" title="">Quisque venenatis ante sit amet dolor</a></li>		
+                            <li><a href="#" title="">Aliquam adipiscing libero vitae leo</a></li>		
+                            <li><a href="#" title="">Sed accumsan quam ac tellus</a></li>	
+                    </ul>
+                    </div>
+
+                    <div class="widget">
+                    <h2>Categories</h2>
+                    <ul>
+                            <li><a href="http://">Design (99+)</a></li>
+                            <li><a href="http://">Web (53)</a></li>
+                            <li><a href="http://">Other (12)</a></li>
+                            <li><a href="http://">Weird (4)</a></li>
+                    </ul>
+                    </div>
+
+                    <div class="widget">
+                    <h2>Meta</h2>
+                    <ul>
+                            <li><a href="">Entries (RSS)</a></li>
+                            <li><a href="">Comments (RSS)</a></li>
+                    </ul>
+                    </div>
+            </aside>
+
+		
+		
+		
+	</div> <!--main-->
+</div>
