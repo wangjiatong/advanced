d63bea281df558b0437a70d78011fa5661b355f3
root <root@S519988.domain>
2016-11-08 17:31:23 +0800

    change the wrong conf file into the original status

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -6,21 +6,6 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=advanced',
-            'username' => 'root',
-            'password' => 'Ww070101',
-            'charset' => 'utf8',
-        ],
-//        'mailer' => [
-//            'class' => 'yii\swiftmailer\Mailer',
-//            'viewPath' => '@common/mail',
-//            // send all mails to a file by default. You have to set
-//            // 'useFileTransport' to false and configure a transport
-//            // for the mailer to send real emails.
-//            'useFileTransport' => true,
-//        ],
     ],
 ];
 
--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=advanced',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
-            'password' => 'Ww070101',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
