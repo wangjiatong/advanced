be5384771de6e80ce10e5d92d7ef860460a0a106
root <root@S519988.domain>
2016-11-08 16:44:26 +0800

    ran a init for a try, no diff while can see

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -6,6 +6,21 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=advanced',
+            'username' => 'root',
+            'password' => 'Ww070101',
+            'charset' => 'utf8',
+        ],
+//        'mailer' => [
+//            'class' => 'yii\swiftmailer\Mailer',
+//            'viewPath' => '@common/mail',
+//            // send all mails to a file by default. You have to set
+//            // 'useFileTransport' to false and configure a transport
+//            // for the mailer to send real emails.
+//            'useFileTransport' => true,
+//        ],
     ],
 ];
 
