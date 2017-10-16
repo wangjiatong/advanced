8a1cd9a6bfd34aebf3d38d6e8f582c061ae8f1e1
root <root@S519988.domain>
2017-10-16 17:43:02 +0800

    个人中心有问题

--- frontend/controllers/WeChatController.php
+++ frontend/controllers/WeChatController.php
@@ -60,7 +60,7 @@ class WeChatController extends Controller
     }
     
     //获取网页授权access_token
-    public function actionAuth($code)
+    public function actionAuth()
     {
 //        $this->layout = 'wx';
         $code = Yii::$app->request->get('code');
